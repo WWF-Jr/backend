1584977dc1ad08cb0524889eb916eac3fcc97ded
NrdyBhu1 <NrdyBhu1@gmail.com>
2021-07-03 13:48:53 +0530

    Add gunicorn

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python main.py
+web: gunicorn --bind 0.0.0.0:5000 wsgi:app
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import os
+# import os
 
 app = Flask(__name__)
 
@@ -12,11 +12,8 @@ def hello_world():
     return "Hello, World!"
 
 
-if os.environ["PORT"]:
-    port = int(os.environ["PORT"])
-else:
-    port = 8000
-
-app.run(host="127.0.0.1", port=port, debug=false)
-
+# if os.environ["PORT"]:
+#     port = int(os.environ["PORT"])
+# else:
+#     port = 8000
 
